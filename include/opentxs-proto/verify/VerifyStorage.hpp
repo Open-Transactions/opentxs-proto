9296f11db72bc9c8c45c2621b4aeffd83f614dc5
Justus Ranvier <justus@opentransactions.org>
2016-01-03 12:08:37 -0600

    Add definitions and verification functions for opentxs::Storage items

---
+++ include/opentxs-proto/verify/VerifyStorage.hpp
@@ -0,0 +1,101 @@
+/************************************************************
+ *
+ *                 OPEN TRANSACTIONS
+ *
+ *       Financial Cryptography and Digital Cash
+ *       Library, Protocol, API, Server, CLI, GUI
+ *
+ *       -- Anonymous Numbered Accounts.
+ *       -- Untraceable Digital Cash.
+ *       -- Triple-Signed Receipts.
+ *       -- Cheques, Vouchers, Transfers, Inboxes.
+ *       -- Basket Currencies, Markets, Payment Plans.
+ *       -- Signed, XML, Ricardian-style Contracts.
+ *       -- Scripted smart contracts.
+ *
+ *  EMAIL:
+ *  fellowtraveler@opentransactions.org
+ *
+ *  WEBSITE:
+ *  http://www.opentransactions.org/
+ *
+ *  -----------------------------------------------------
+ *
+ *   LICENSE:
+ *   This Source Code Form is subject to the terms of the
+ *   Mozilla Public License, v. 2.0. If a copy of the MPL
+ *   was not distributed with this file, You can obtain one
+ *   at http://mozilla.org/MPL/2.0/.
+ *
+ *   DISCLAIMER:
+ *   This program is distributed in the hope that it will
+ *   be useful, but WITHOUT ANY WARRANTY; without even the
+ *   implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *   PARTICULAR PURPOSE.  See the Mozilla Public License
+ *   for more details.
+ *
+ ************************************************************/
+
+#ifndef OPENTXS_PROTO_VERIFYSTORAGE_HPP
+#define OPENTXS_PROTO_VERIFYSTORAGE_HPP
+
+#ifdef _WIN32
+#pragma warning(push)
+#pragma warning(disable : 4244)
+#pragma warning(disable : 4267)
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
+#ifndef __clang__
+// -Wuseless-cast does not exist in clang
+#pragma GCC diagnostic ignored "-Wuseless-cast"
+#endif
+#endif
+
+#ifdef OPENTXS_PROTO_INTERNAL_BUILD
+#include "StorageRoot.pb.h"
+#include "StorageItems.pb.h"
+#include "VerifyCredentials.hpp"
+#else
+#include <opentxs-proto/StorageRoot.pb.h>
+#include <opentxs-proto/StorageItems.pb.h>
+#include <opentxs-proto/verify/VerifyCredentials.hpp>
+#endif
+
+#ifdef _WIN32
+#pragma warning(pop)
+#else
+#pragma GCC diagnostic pop
+#endif
+
+namespace opentxs { namespace proto
+{
+    static const VersionMap StorageItemsAllowedCredential =
+        {
+            { 1, {1, 1}},
+        };
+    static const VersionMap StorageCredentialAllowedHash =
+        {
+            { 1, {1, 1}},
+        };
+
+    bool Verify(
+        const StorageRoot& root,
+        const uint32_t minVersion = 0,
+        const uint32_t maxVersion = 0xffffffff);
+    bool Verify(
+        const StorageItems& items,
+        const uint32_t minVersion = 0,
+        const uint32_t maxVersion = 0xffffffff);
+    bool Verify(
+        const StorageCredentials& creds,
+        const uint32_t minVersion = 0,
+        const uint32_t maxVersion = 0xffffffff);
+    bool Verify(
+        const StorageItemHash& hash,
+        const uint32_t minVersion = 0,
+        const uint32_t maxVersion = 0xffffffff);
+
+} // namespace proto
+} // namespace opentxs
+#endif // OPENTXS_PROTO_VERIFYSTORAGE_HPP
---
+++ src/verify/storageitemhash/StorageItemHash_1.cpp
@@ -0,0 +1,70 @@
+/************************************************************
+ *
+ *                 OPEN TRANSACTIONS
+ *
+ *       Financial Cryptography and Digital Cash
+ *       Library, Protocol, API, Server, CLI, GUI
+ *
+ *       -- Anonymous Numbered Accounts.
+ *       -- Untraceable Digital Cash.
+ *       -- Triple-Signed Receipts.
+ *       -- Cheques, Vouchers, Transfers, Inboxes.
+ *       -- Basket Currencies, Markets, Payment Plans.
+ *       -- Signed, XML, Ricardian-style Contracts.
+ *       -- Scripted smart contracts.
+ *
+ *  EMAIL:
+ *  fellowtraveler@opentransactions.org
+ *
+ *  WEBSITE:
+ *  http://www.opentransactions.org/
+ *
+ *  -----------------------------------------------------
+ *
+ *   LICENSE:
+ *   This Source Code Form is subject to the terms of the
+ *   Mozilla Public License, v. 2.0. If a copy of the MPL
+ *   was not distributed with this file, You can obtain one
+ *   at http://mozilla.org/MPL/2.0/.
+ *
+ *   DISCLAIMER:
+ *   This program is distributed in the hope that it will
+ *   be useful, but WITHOUT ANY WARRANTY; without even the
+ *   implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *   PARTICULAR PURPOSE.  See the Mozilla Public License
+ *   for more details.
+ *
+ ************************************************************/
+
+#include "opentxs-proto/verify/StorageItemHash.hpp"
+
+#include <iostream>
+
+namespace opentxs { namespace proto
+{
+
+bool StorageItemHash_1(
+    const StorageItemHash& hash)
+{
+    if (!hash.has_itemid()) {
+        std::cerr << "Verify serialized storage item hash failed: missing identifier." << std::endl;
+        return false;
+    }
+    if (MIN_PLAUSIBLE_IDENTIFIER > hash.itemid().size()) {
+        std::cerr << "Verify serialized storage item hash failed: invalid identifier." << std::endl;
+        return false;
+    }
+    if (!hash.has_hash()) {
+        std::cerr << "Verify serialized storage item hash failed: missing hash." << std::endl;
+        return false;
+    }
+    if (MIN_PLAUSIBLE_IDENTIFIER > hash.hash().size()) {
+        std::cerr << "Verify serialized storage item hash failed: invalid hash." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+} // namespace proto
+} // namespace opentxs
