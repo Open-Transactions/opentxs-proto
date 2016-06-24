064cd3be77c6d189cde859898db6d29cbe4c5938
Justus Ranvier <justus@opentransactions.org>
2016-06-24 01:14:13 -0500

    add support for building Python bindings

---
+++ src/opentxs-proto/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='opentxsproto',
+      version='0.1',
+      description='Open-Transactions Canonical Protbuf Definition Library',
+      author='Justus Ranvier',
+      author_email='justus@opentransactions.org',
+      url='https://github.com/Open-Transactions/opentxs-proto',
+      packages=['opentxsproto'],
+     )
