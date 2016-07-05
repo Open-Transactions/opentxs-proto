9e9ff5594b897e2b6dcd1762f558894f247aeae6
Justus Ranvier <justus@opentransactions.org>
2016-07-05 18:25:15 -0500

    Add support for peer request/reply boxes to StorageNym

--- src/verify/storagenym/StorageNym_1.cpp
+++ src/verify/storagenym/StorageNym_1.cpp
@@ -47,13 +47,92 @@ bool CheckProto_1(
     const StorageNym& nym)
 {
     if (nym.has_credlist()) {
-        bool validCredlist = Check(
+        bool valid = Check(
             nym.credlist(),
             StorageNymAllowedHash.at(nym.version()).first,
             StorageNymAllowedHash.at(nym.version()).second);
 
-        if (!validCredlist) {
-            std::cerr << "Verify serialized storage nym failed: invalid credential list." << std::endl;
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "credential list." << std::endl;
+            return false;
+        }
+    }
+
+    if (nym.has_sentpeerrequests()) {
+        bool valid = Check(
+            nym.sentpeerrequests(),
+            StorageNymAllowedHash.at(nym.version()).first,
+            StorageNymAllowedHash.at(nym.version()).second);
+
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "sentpeerrequests." << std::endl;
+            return false;
+        }
+    }
+
+    if (nym.has_incomingpeerrequests()) {
+        bool valid = Check(
+            nym.incomingpeerrequests(),
+            StorageNymAllowedHash.at(nym.version()).first,
+            StorageNymAllowedHash.at(nym.version()).second);
+
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "incomingpeerrequests." << std::endl;
+            return false;
+        }
+    }
+
+    if (nym.has_sentpeerreply()) {
+        bool valid = Check(
+            nym.sentpeerreply(),
+            StorageNymAllowedHash.at(nym.version()).first,
+            StorageNymAllowedHash.at(nym.version()).second);
+
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "sentpeerreply." << std::endl;
+            return false;
+        }
+    }
+
+    if (nym.has_incomingpeerreply()) {
+        bool valid = Check(
+            nym.incomingpeerreply(),
+            StorageNymAllowedHash.at(nym.version()).first,
+            StorageNymAllowedHash.at(nym.version()).second);
+
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "incomingpeerreply." << std::endl;
+            return false;
+        }
+    }
+
+    if (nym.has_finishedpeerrequest()) {
+        bool valid = Check(
+            nym.finishedpeerrequest(),
+            StorageNymAllowedHash.at(nym.version()).first,
+            StorageNymAllowedHash.at(nym.version()).second);
+
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "finishedpeerrequest." << std::endl;
+            return false;
+        }
+    }
+
+    if (nym.has_finishedpeerreply()) {
+        bool valid = Check(
+            nym.finishedpeerreply(),
+            StorageNymAllowedHash.at(nym.version()).first,
+            StorageNymAllowedHash.at(nym.version()).second);
+
+        if (!valid) {
+            std::cerr << "Verify serialized storage nym failed: invalid "
+                      << "finishedpeerreply." << std::endl;
             return false;
         }
     }
