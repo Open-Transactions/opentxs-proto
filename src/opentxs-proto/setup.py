a5deed682e3a4d9ee2ff0e76daef4010461a3f2d
Jay Carlson <nop@nop.com>
2018-10-08 15:56:41 -0400

    Make opentxsproto Python bindings behave more like a Python package
    Currently, the `opentxsproto` directory needs to go on your Python `PATH`; this isn't very Pythonic. This PR rewrites *Protobuf-generated* files to switch from unqualified `import Foo_pb2` to `import opentxsproto.Foo_pb2`. Usage is then:
    
    ```python
    import opentxsproto
    
    br = opentxsproto.BailmentReply_pb2.BailmentReply()
    ```
    
    Without a modified `__init__.py`, `from opentxsproto import *` won't work. This PR also generates an `__init__.py` which in turn imports each binding file into the `opentxsproto` namespace. You can still use the style above, but it also allows:
    
    ```python
    import opentxsproto as pb
    
    br = pb.BailmentReply()
    ```
    
    Ultimately, some kind of change to how the Protobuf compiler is invoked seems necessary, but this works for now.
    
    The version number is bumped.

--- src/opentxs-proto/setup.py
+++ src/opentxs-proto/setup.py
@@ -1,14 +1,71 @@
+#!/usr/bin/env python
 # Copyright (c) 2018 The Open-Transactions developers
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#!/usr/bin/env python
+"""This post-processing creates something closer to an idiomatic
+Python package.
 
+Usage:
+
+import opentxsproto as pb
+
+mf = pb.MoveFunds()
+
+MoveFunds_pb2 (and the internal-only MoveFunds__pb2) are still available.
+
+"""
+
+import glob
+import re
 from setuptools import setup
 
+PREFIX = "opentxsproto"
+
+# If we're in an opentxsproto package, we need to qualify internal imports
+IMPORT_SIBLING = re.compile(r"\nimport (\w+) as (\w+)")
+LOCAL_FROM_IMPORT = re.compile(r"\nfrom ([A-Z]\w+) import \*")
+
+def rewrite_file(filename):
+    """Rewrite a file, replacing imports with package-specific imports"""
+    with open(filename) as read_file:
+        whole_file_string = read_file.read()
+
+    whole_file_string = IMPORT_SIBLING.sub(
+        r"\nimport opentxsproto.\1 as \2", whole_file_string)
+    whole_file_string = LOCAL_FROM_IMPORT.sub(
+        r"\nfrom opentxsproto.\1 import *", whole_file_string)
+
+    with open(filename, "w") as write_file:
+        write_file.write(whole_file_string)
+
+
+MODULE_FROM_FILENAME = re.compile(f"{PREFIX}/(\\w+)_pb2.py")
+
+def extract_module_name_from_filename(m):
+    """Given a filename from the module, extract the module name"""
+    res = MODULE_FROM_FILENAME.match(m)
+    assert res
+    return res.group(1)
+
+MODULES = []
+
+# Rewrite files, saving module names
+for f in glob.glob(f"{PREFIX}/*_pb2.py"):
+    rewrite_file(f)
+    module = extract_module_name_from_filename(f)
+    MODULES.append(module)
+
+
+# Generate an __init__ which imports all the modules.
+with open("opentxsproto/__init__.py", "w") as f:
+    for module in MODULES:
+        # f.write(f"import {PREFIX}.{module}_pb2 as {module}\n")
+        f.write(f"from {PREFIX}.{module}_pb2 import *\n")
+
 setup(name='opentxsproto',
-      version='0.1',
+      version='0.2',
       description='Open-Transactions Canonical Protbuf Definition Library',
       author='Justus Ranvier',
       author_email='justus@opentransactions.org',
