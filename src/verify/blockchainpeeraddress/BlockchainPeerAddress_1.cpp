a8c1925cd73d15a8f6e59268c42ca807f9612563
Justus Ranvier <justus@opentransactions.org>
2020-01-01 08:54:02 -0600

    Define message for serialized blockchain peer addresses

---
+++ src/verify/blockchainpeeraddress/BlockchainPeerAddress_1.cpp
@@ -0,0 +1,118 @@
+// Copyright (c) 2019 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "opentxs-proto/Types.hpp"
+#include "verify/Check.hpp"
+
+#define PROTO_NAME "blockchain peer address"
+
+namespace opentxs
+{
+namespace proto
+{
+bool CheckProto_1(const BlockchainPeerAddress& input, const bool silent)
+{
+    CHECK_IDENTIFIER(id);
+    CHECK_EXISTS(address);
+
+    return true;
+}
+
+bool CheckProto_2(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(2)
+}
+
+bool CheckProto_3(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(3)
+}
+
+bool CheckProto_4(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(4)
+}
+
+bool CheckProto_5(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(5)
+}
+
+bool CheckProto_6(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(6)
+}
+
+bool CheckProto_7(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(7)
+}
+
+bool CheckProto_8(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(8)
+}
+
+bool CheckProto_9(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(9)
+}
+
+bool CheckProto_10(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(10)
+}
+
+bool CheckProto_11(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(11)
+}
+
+bool CheckProto_12(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(12)
+}
+
+bool CheckProto_13(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(13)
+}
+
+bool CheckProto_14(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(14)
+}
+
+bool CheckProto_15(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(15)
+}
+
+bool CheckProto_16(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(16)
+}
+
+bool CheckProto_17(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(17)
+}
+
+bool CheckProto_18(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(18)
+}
+
+bool CheckProto_19(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(19)
+}
+
+bool CheckProto_20(const BlockchainPeerAddress& input, const bool silent)
+{
+    UNDEFINED_VERSION(20)
+}
+}  // namespace proto
+}  // namespace opentxs
